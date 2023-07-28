7e264cd955a2df7c723a8cf0c83da619d491861e
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-07-28 03:58:15 +0000

    added new caar

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,34 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand= brand;
+    this._color = color;
+    this._motor = motor;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  get motor() [
+     return this._motor;
+  }
+  set color(newColor) {
+    this._color = newColor;
+  }
+
+  set brand(newBrand) {
+    this._brand= newBrand;
+  }
+  
+  set motor(newMotor) {
+     this._motor = newMotor;
+  }
+
+  CloneCar() {
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
