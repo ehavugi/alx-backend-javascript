ae2c9890da07df32b13f01937bf0accb39284b5f
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-08-02 03:46:00 +0000

    style fixes for groceries and uniq items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,10 +1,10 @@
 export default function updateUniqueItems(items) {
   const mapiter = items.keys();
-  for (let i = 0; i < items.size; i++) {
-	 const keyi = mapiter.next().value;
-	 if (items.get(keyi) == 1) {
-	   items.set(keyi, 100);
-	 }
+  for (let i = 0; i < items.size; i += 1) {
+    const keyi = mapiter.next().value;
+    if (items.get(keyi) === 1) {
+      items.set(keyi, 100);
+    }
   }
 
   return items;
