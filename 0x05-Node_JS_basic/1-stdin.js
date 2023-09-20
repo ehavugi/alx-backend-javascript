4baaafab6d2c8f0a07fce46a45e1fb8a06550c92
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-20 02:11:46 +0000

    fixed on program ending handling

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,6 +4,9 @@ process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
-    console.log('This important software is now closing');
   }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
