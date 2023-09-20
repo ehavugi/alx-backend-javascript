320e00b04e11bfa7923099c0ee1e908dc220be88
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-20 03:59:23 +0000

    fixed http server

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,13 +1,20 @@
 const http = require('http');
 
 const hostname = 'localhost';
-
 const port = 1245;
-
 const app = http.createServer((req, res) => {
   res.status = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!');
+  const { url } = req;
+  console.log(url);
+  if (url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.write(process.argv[process.argv.length - 1]);
+    res.end(' =>students here are');
+  } else {
+    res.end('Other');
+  }
 });
 
 app.listen(port, hostname, () => {
